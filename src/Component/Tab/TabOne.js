900062b8591ffa52638ad297547232f5adf8fc26
liuminghao <liuminghao@benlai.com>
2019-10-11 19:35:02 +0800

    learn object 面向对象

--- src/Component/Tab/TabOne.js
+++ src/Component/Tab/TabOne.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 class TabOne extends Component{
   constructor(props) {
@@ -9,8 +10,94 @@ class TabOne extends Component{
 
   render() {
     return (
-      <div>
-        我是Tab页面 1
+      <div style={{height: '100%'}}>
+        <Scrollbars style={{ width: 500, height: '100%' }}>
+          <div className="scroll-text">
+            我是Tab页面 1我是Tab页面 1我是Tab页面 1我是Tab页面 1
+            导航菜单过长时，需要滚动：
+
+            在Sider标签里加上就可以了，当长度超过屏幕高度时就能滚动，出现滚动条，没有超过时，就没有滚动条；
+
+            但是默认的滚动条又不美观，就需要美化一下：
+
+            先安装一个依赖：yarn add react-custom-scrollbars
+
+            然后：importfrom 'react-custom-scrollbars';
+
+            接着在你需要滚动的地方用包起来；
+            我是Tab页面 1我是Tab页面 1我是Tab页面 1我是Tab页面 1
+            导航菜单过长时，需要滚动：
+
+            在Sider标签里加上就可以了，当长度超过屏幕高度时就能滚动，出现滚动条，没有超过时，就没有滚动条；
+
+            但是默认的滚动条又不美观，就需要美化一下：
+
+            先安装一个依赖：yarn add react-custom-scrollbars
+
+            然后：importfrom 'react-custom-scrollbars';
+
+            接着在你需要滚动的地方用包起来；
+
+            finish。
+            ————————————————
+            版权声明：本文为CSDN博主「my小新」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+            原文链接：https://blog.csdn.net/weixin_38304202/article/details/89947991我是Tab页面 1我是Tab页面 1我是Tab页面 1我是Tab页面 1
+            导航菜单过长时，需要滚动：
+
+            在Sider标签里加上就可以了，当长度超过屏幕高度时就能滚动，出现滚动条，没有超过时，就没有滚动条；
+
+            但是默认的滚动条又不美观，就需要美化一下：
+
+            先安装一个依赖：yarn add react-custom-scrollbars
+
+            然后：importfrom 'react-custom-scrollbars';
+
+            接着在你需要滚动的地方用包起来；
+
+            finish。
+            ————————————————
+            版权声明：本文为CSDN博主「my小新」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+            原文链接：https://blog.csdn.net/weixin_38304202/article/details/89947991
+            finish。
+            ————————————————
+            版权声明：本文为CSDN博主「my小新」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+            原文链接：https://blog.csdn.net/weixin_38304202/article/details/89947991
+            我是Tab页面 1我是Tab页面 1我是Tab页面 1我是Tab页面 1
+            导航菜单过长时，需要滚动：
+
+            在Sider标签里加上就可以了，当长度超过屏幕高度时就能滚动，出现滚动条，没有超过时，就没有滚动条；
+
+            但是默认的滚动条又不美观，就需要美化一下：
+
+            先安装一个依赖：yarn add react-custom-scrollbars
+
+            然后：importfrom 'react-custom-scrollbars';
+
+            接着在你需要滚动的地方用包起来；
+
+            finish。
+            ————————————————
+            版权声明：本文为CSDN博主「my小新」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+            原文链接：https://blog.csdn.net/weixin_38304202/article/details/89947991
+            我是Tab页面 1我是Tab页面 1我是Tab页面 1我是Tab页面 1
+            导航菜单过长时，需要滚动：
+
+            在Sider标签里加上就可以了，当长度超过屏幕高度时就能滚动，出现滚动条，没有超过时，就没有滚动条；
+
+            但是默认的滚动条又不美观，就需要美化一下：
+
+            先安装一个依赖：yarn add react-custom-scrollbars
+
+            然后：importfrom 'react-custom-scrollbars';
+
+            接着在你需要滚动的地方用包起来；
+
+            finish。
+            ————————————————
+            版权声明：本文为CSDN博主「my小新」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+            原文链接：https://blog.csdn.net/weixin_38304202/article/details/89947991
+          </div>
+        </Scrollbars>
       </div>
     )
   }
